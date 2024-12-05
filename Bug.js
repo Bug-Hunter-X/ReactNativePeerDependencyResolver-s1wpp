8ffe94d821a95f1ff7a884ed1a825eb5e423ba53
BugHunter-X <askzenai@gmail.com>
2024-12-05 15:58:06 +0100

    Add Bug.js file

---
+++ Bug.js
@@ -0,0 +1,1 @@
+This error occurs when you are using a third party library that has peerDependencies that are not installed. The error message may not be very clear, but it usually involves a message about missing modules or dependencies. 
